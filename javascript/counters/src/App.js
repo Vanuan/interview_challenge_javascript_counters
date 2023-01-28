edd40261665e781565af01c03102adeb40260943
Vanuan <vanuan@gmail.com>
2023-01-28 06:48:34 +0200

    Stage 1

--- javascript/counters/src/App.js
+++ javascript/counters/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
 const data = [
   { id: 1, value: 0 },
@@ -7,23 +7,45 @@ const data = [
 ];
 
 function Counter(props) {
-  const { value } = props;
+  const { id, initialValue, onChange } = props;
+  const [value, setValue] = useState(initialValue);
+
+  const handleIncrement = () => {
+    setValue(value + 1);
+    onChange(id, value + 1);
+  };
+
+  const handleDecrement = () => {
+    setValue(value - 1);
+    onChange(id, value - 1);
+  };
+
   return (
     <div>
       <b>{value}</b>
       <div>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
       </div>
     </div>
   );
 }
 
 function App() {
+  const handleCounterChange = useCallback((id, value) => {
+    data.find((counter) => counter.id === id).value = value;
+    console.log(data);
+  }, []);
+
   return (
     <div>
       {data.map((counter) => (
-        <Counter key={counter.id} value={counter.value} />
+        <Counter
+          key={counter.id}
+          id={counter.id}
+          initialValue={counter.value}
+          onChange={handleCounterChange}
+        />
       ))}
     </div>
   );
